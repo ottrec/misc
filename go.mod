479e83e6c787d06b241f90d49cba326b0862055e
Patrick Gaskin <patrick@pgaskin.net>
2025-12-19 06:25:11 -0500

    stats/l1swim: Initial commit

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.25.0
 require (
 	github.com/chromedp/cdproto v0.0.0-20250724212937-08a3db8b4327
 	github.com/chromedp/chromedp v0.14.2
+	github.com/pgaskin/ottrec v0.0.0-20251007032526-ab79bc674a9a
 	github.com/pgaskin/ottrec-website v0.0.0-20251115210714-1c1ec9843b7d
 )
 
@@ -17,7 +18,6 @@ require (
 	github.com/kelindar/bitmap v1.5.3 // indirect
 	github.com/kelindar/simd v1.1.2 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
-	github.com/pgaskin/ottrec v0.0.0-20251007032526-ab79bc674a9a // indirect
 	golang.org/x/sys v0.36.0 // indirect
 	google.golang.org/protobuf v1.36.10 // indirect
 )
---
+++ stats/l1swim/main.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"cmp"
+	"context"
+	"flag"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/pgaskin/ottrec-website/pkg/ottrecdl"
+	"github.com/pgaskin/ottrec-website/pkg/ottrecidx"
+	"github.com/pgaskin/ottrec/schema"
+)
+
+func main() {
+	flag.Parse()
+
+	ctx := context.Background()
+
+	cl := &ottrecdl.Client{
+		Base: "https://data.ottrec.ca/",
+	}
+
+	pb, err := cl.Get(ctx, cmp.Or(flag.Arg(0), "latest"), "pb")
+	if err != nil {
+		panic(err)
+	}
+
+	idx, err := new(ottrecidx.Indexer).Load(pb)
+	if err != nil {
+		panic(err)
+	}
+
+	// aquatic facilities within a short (~15 minute fast walk, or ~10 minute
+	// bus at least every 20 min) distance of a Line 1 station.
+	line1PoolName := []string{
+		"plant",
+		"jack purcell",
+		"champagne",
+		"st. laurent",
+		"splash",
+		"bob macquarrie",
+	}
+	line1Pool := make([]ottrecidx.FacilityRef, len(line1PoolName))
+	for i, name := range line1PoolName {
+		for fac := range idx.Data().Facilities() {
+			if strings.Contains(strings.ToLower(fac.GetName()), name) {
+				line1Pool[i] = fac
+				break
+			}
+		}
+	}
+	for i, fac := range line1Pool {
+		if !fac.Valid() {
+			panic(fmt.Errorf("no match for facility %q", line1PoolName[i]))
+		}
+	}
+
+	line1Swim := make([]ottrecidx.ScheduleGroupRef, len(line1Pool))
+	for i, fac := range line1Pool {
+		for grp := range fac.ScheduleGroups() {
+			if strings.Contains(strings.ToLower(grp.GetLabel()), "swim") {
+				if line1Swim[i].Valid() {
+					panic(fmt.Errorf("multiple swim schedule groups for facility %q (%q, %q)", fac.GetName(), line1Swim[i].GetLabel(), grp.GetLabel()))
+				}
+				line1Swim[i] = grp
+			}
+		}
+	}
+	for i, grp := range line1Swim {
+		if !grp.Valid() {
+			panic(fmt.Errorf("no match for facility %q schedule group %q", line1PoolName[i], "swim"))
+		}
+	}
+
+	for i, grp := range line1Swim {
+		if i != 0 {
+			fmt.Println()
+		}
+		if grp.Schedules().Empty() {
+			fmt.Printf("%q :: %q :: %s\n", grp.Facility().GetName(), grp.GetLabel(), "no schedules in group, skipping")
+			continue
+		}
+		for sch := range grp.Schedules() {
+			fmt.Println()
+
+			dr, _ := sch.GetDateRange()
+			fmt.Printf("%q :: %q :: %s\n", grp.Facility().GetName(), grp.GetLabel(), dr)
+
+			var morn, after, eve []ottrecidx.TimeRef
+			for tm := range sch.Times() {
+				if strings.Contains(tm.Activity().GetName(), "lane swim") {
+					cr, _ := tm.GetRange()
+					if cr.Overlaps(schema.MakeClockRange(18, 00, 23, 59)) {
+						eve = append(eve, tm)
+					} else if cr.Overlaps(schema.MakeClockRange(11, 30, 18, 00)) {
+						after = append(after, tm)
+					} else {
+						morn = append(morn, tm)
+					}
+				}
+			}
+			format := func(r []ottrecidx.TimeRef) string {
+				var b strings.Builder
+				var t time.Duration
+				w := make(map[time.Weekday]struct{}, 7)
+				for i, x := range r {
+					if i != 0 {
+						b.WriteString(", ")
+					}
+					cr, _ := x.GetRange()
+					wd, _ := x.GetWeekday()
+					w[wd] = struct{}{}
+					b.WriteString(wd.String()[:2])
+					b.WriteString(" ")
+					b.WriteString(cr.Start.Format(false))
+					t += (time.Duration(cr.End) - time.Duration(cr.Start)) * time.Minute
+				}
+				b.WriteString(" (")
+				b.WriteString(t.String())
+				b.WriteString(" total, ")
+				b.WriteString(strconv.Itoa(len(w)))
+				b.WriteString(" weekdays)")
+				return b.String()
+			}
+
+			fmt.Printf("  morning:   %s\n", format(morn))
+			fmt.Printf("  afternoon: %s\n", format(after))
+			fmt.Printf("  evening:   %s\n", format(eve))
+		}
+	}
+}
